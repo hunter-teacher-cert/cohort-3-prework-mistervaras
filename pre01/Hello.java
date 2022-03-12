632b781051fb544e9fc637ff50f348819d0a83d0
mistervaras <96734779+mistervaras@users.noreply.github.com>
2022-03-12 17:16:49 +0000

    Pre01 creating a space between text

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Hello{
   public static void main(String [] args){
-    System.out.println("A Polite Greeting is");
-    System.out.print("");
-    System.out.println("Hello World!");
+    System.out.print("A Polite Greeting is");
+    System.out.print(" ");
+    System.out.print("Hello World!");
     System.out.println("How are you?");
 
   }
