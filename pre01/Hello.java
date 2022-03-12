b626073721699b0d2b8b1fa78b454bfb8695a6f5
mistervaras <96734779+mistervaras@users.noreply.github.com>
2022-03-12 17:41:44 +0000

    Spaces between letters

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -7,6 +7,17 @@ public class Hello{
     System.out.print(" ");
     System.out.print("Hello World!");
     System.out.println("How are you?");
+    System.out.println("Hello world.");
+        System.out.println("Hello World!");
+        System.out.print("H");
+        System.out.print(" ");
+        System.out.print("e");
+        System.out.print(" ");
+        System.out.print("l");
+        System.out.print(" ");
+        System.out.print("l");
+        System.out.print(" ");
+        System.out.print("o");
 
   }
 }
