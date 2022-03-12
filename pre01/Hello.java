ce30b5bf18bd7cdd38c5ac8a8768f3bc12669d11
mistervaras <96734779+mistervaras@users.noreply.github.com>
2022-03-12 16:10:29 +0000

    Experimenting with print vs println

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -3,7 +3,10 @@ import java.util.*;
 
 public class Hello{
   public static void main(String [] args){
+    System.out.println("A Polite Greeting is");
+    System.out.print("");
     System.out.println("Hello World!");
+    System.out.println("How are you?");
 
   }
 }
---
+++ pre04/Conditional.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Conditional{
+
+  public static void main(String [] args){
+
+   int a;
+
+    a = 2
+      0;
+
+      if (a > 10)
+        System.out.println("A is bigger");
+  }
+}
