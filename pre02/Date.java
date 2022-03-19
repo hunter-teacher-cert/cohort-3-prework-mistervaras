3b5a3998c22a8062f3591e58da4689e02ebb932f
mistervaras <96734779+mistervaras@users.noreply.github.com>
2022-03-19 20:31:49 +0000

    update pre02 for assignment 2.12

---
+++ pre02/Date.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Date{
+  public static void main(String [] args){
+
+  String day = "Saturday, ";
+  String date = " 19,";
+  String month = "March"; 
+  String year = "2022";
+    
+    
+    System.out.println("American format:"); 
+    System.out.print(day);
+    System.out.print(month);
+    System.out.print(date);
+    System.out.println(year);
+    
+
+    }
+}
