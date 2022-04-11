f93c243e76c811f19a0649bec48f868ca96304f1
mistervaras <96734779+mistervaras@users.noreply.github.com>
2022-04-11 23:04:56 +0000

    Time from midnight

---
+++ pre02/Time.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Time{
+public static void main(String[] args){
+
+  int hour = 11;
+int minute = 59;
+System.out.print("Number of minutes since midnight: ");
+System.out.println(hour * 60 + minute);
+  
+}
+
+}
+
+
+
+  
